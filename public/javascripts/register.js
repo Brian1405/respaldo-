d977a1be93eab459775ae078c68dddc16961cf0c
Bramirezsm14@gmail.com <bramirezsm14@gmail.com>
2020-06-15 23:16:54 -0300

    respaldo

---
+++ public/javascripts/register.js
@@ -0,0 +1,27 @@
+const { check } = require("express-validator")
+
+const form =document.querySelector('.form')
+const nombre = document.querySelector('#nombre')
+const apellido = document.querySelector('#apellido')
+const email = document.querySelector('email')
+const contraseña = document.querySelector('contraseña')
+const rcontraseña= document.querySelector('rcontraseña')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    checkInputs()
+})
+//evitando espacio 
+function checkInputs(){
+    const nombre=nombre.value.trim()
+    const apellidoValue=apellido.value.trim()
+    const emailValue=email.value.trim()
+    const contraseñaValue=contraseña.value.trim()
+    const rcontraseñaValue=rcontraseña.value.trim()
+ 
+    nombre===''?setError(nombre,'El campo no puede estar vacio'):''
+}
+function setError(input,message){
+    const formControl=input.parentElement
+    formControl.querySelector('.error').innerHTML=message
+}
